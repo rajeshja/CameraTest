020bd9faceb0e106128916430600913eda1120b7
Rajesh J Advani <rajesh.jag@wipro.com>
2010-12-06 19:53:43 +0530

    First attempt at adding camera preview to custom app. On emulator, fails to connect to camera service.

--- src/rja/android/camera/CameraTest.java
+++ src/rja/android/camera/CameraTest.java
@@ -2,32 +2,32 @@ package rja.android.camera;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
-import android.hardware.Camera.Size;
 import android.widget.TextView;
-
-import java.util.List;
 
 public class CameraTest extends Activity
 {
+	CameraView cameraView;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-		Camera camera = Camera.open();
-		Parameters cameraParams = camera.getParameters();
-		List<Size> sizes = cameraParams.getSupportedPreviewSizes();
-		
-		StringBuffer sb = new StringBuffer();
-		for(Size size: sizes) {
-			sb.append(size.width + "x" + size.height + ", ");
-		}
-
-		TextView label = (TextView) findViewById(R.id.text);
-		label.setText(sb.toString());
-		
+		cameraView = (CameraView) findViewById(R.id.camera_preview);
     }
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+		cameraView.releaseCamera();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		
+		cameraView.reopenCamera();
+	}
 }
--- src/rja/android/camera/CameraView.java
+++ src/rja/android/camera/CameraView.java
@@ -1,8 +1,15 @@
 package rja.android.camera;
 
 import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
+import android.util.AttributeSet;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Describe class CameraView here.
@@ -15,22 +22,79 @@ import android.view.SurfaceHolder;
  */
 public class CameraView extends SurfaceView implements SurfaceHolder.Callback {
 
-	CameraView(Context context) {
+	private Camera camera;
+
+	public CameraView(Context context) {
 		super(context);
 		
 		getHolder().addCallback(this);
 		getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
 
-	public void  surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-		
+
+	public CameraView(Context context, AttributeSet  attrs) {
+		super(context, attrs);
+
+		getHolder().addCallback(this);
+		getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+	
+	public CameraView(Context context, AttributeSet  attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		getHolder().addCallback(this);
+		getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+
+	public void releaseCamera() {
+		if (camera != null) {
+			camera.release();
+			camera = null;
+		}
+	}
+
+	public void reopenCamera() {
+		camera = camera.open();
 	}
 
 	public void  surfaceCreated(SurfaceHolder holder) {
+		Camera camera = Camera.open();
+		try {
+			camera.setPreviewDisplay(getHolder());
+		} catch (IOException e) {
+			releaseCamera();
+		}
+	}
 
+	public void  surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+		Parameters cameraParams = camera.getParameters();
+		List<Size> sizes = cameraParams.getSupportedPreviewSizes();
+
+		Size selectedSize;
+
+		if ((sizes != null) && (sizes.size()>=0)) {
+			selectedSize = sizes.get(0);
+		} else {
+			selectedSize = null;
+		}
+
+		for(Size size: sizes) {
+			if ((size.width <= width) && (size.height <= height)) {
+				if ((selectedSize == null)
+					|| ((selectedSize.width < size.width)
+						&& (selectedSize.height < size.height))) {
+					selectedSize = size;
+				}
+			}
+		}
+
+		cameraParams.setPreviewSize(selectedSize.width, selectedSize.height);
+		camera.setParameters(cameraParams);
+		camera.startPreview();
 	}
 
 	public void  surfaceDestroyed(SurfaceHolder holder) {
-		
+		camera.stopPreview();
+		releaseCamera();
 	}
 }
